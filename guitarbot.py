e506c4f9e238e56fb5e10f00cb2e2486feb22f72
Carlos Daniel Oliveira <carlosdanielbarbosadeoliveira@gmail.com>
2024-12-22 23:24:06 -0300

    BOT

---
+++ guitarbot.py
@@ -0,0 +1,25 @@
+from time import sleep 
+import keyboard
+import pyautogui as pa
+
+
+
+
+while keyboard.is_pressed(1) == False:
+    if pa.pixelMatchesColor(749,812,(0,152,0)) :
+        pa.press('a')
+        
+
+    if pa.pixelMatchesColor(848,813,(255,0,0)) : 
+        pa.press('s')
+        
+
+    if pa.pixelMatchesColor(945,815,(244,244,2)) :
+        pa.press('j')
+         
+
+    if pa.pixelMatchesColor(1043,813,(0,152,255)): 
+        pa.press('k')
+        
+
+
